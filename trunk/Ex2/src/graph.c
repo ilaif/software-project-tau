722f92e99f7abe153ff42ef9ad8034d99631b0b9
mo.aviv@gmail.com <mo.aviv@gmail.com@e2e18dd1-964b-1405-b696-49bddae1d5f7>
2011-12-19 07:53:29 +0000

    Initial import.

---
+++ trunk/Ex2/src/graph.c
@@ -0,0 +1,34 @@
+
+#include "graph.h"
+
+void add_vertex(char* name) {
+
+}
+
+void remove_vertex_by_id(int id) {
+
+}
+
+void remove_vertex_by_name(char* name) {
+
+}
+
+void add_edge_by_name(char* first_vertex_name, char* second_vertex_name, float weight) {
+
+}
+
+void add_edge_by_id(int head_vertex_id, int tail_vertex_id, float weight) {
+
+}
+
+void remove_edge(int id) {
+
+}
+
+void print() {
+
+}
+
+void cluster(int num_of_clusters) {
+
+}
---
+++ trunk/Ex2/src/graph.h
@@ -0,0 +1,24 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "types.h"
+
+/* Include declaration for graph methods */
+
+void add_vertex(char* name);
+
+void remove_vertex_by_id(int id);
+
+void remove_vertex_by_name(char* name);
+
+void add_edge_by_name(char* first_vertex_name, char* second_vertex_name, float weight);
+
+void add_edge_by_id(int head_vertex_id, int tail_vertex_id, float weight);
+
+void remove_edge(int id);
+
+void print();
+
+void cluster(int num_of_clusters);
+
+#endif
